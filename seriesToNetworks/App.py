8f4ed3fe978da956cb86a7f88679479f13d310d2
Vinicius de Paula <viniciusdepaula199@gmail.com>
2021-12-09 16:02:59 -0300

    add results

--- seriesToNetworks/App.py
+++ seriesToNetworks/App.py
@@ -13,35 +13,19 @@ from scipy import stats
 import sklearn.metrics as sm
 import sklearn.feature_selection._mutual_info as mmmi
 
-# ruido em uma freq (gaussiana)
-# distribuíção uniforme
-# intensidade (de 0 até 1 vezes a intenção)
-
-#caotico + periódico
-
-# pega uma métrica (dtw) altera os valores de i
-# e ve oq q da a forma da curva
-# para cada i 1000 séries.
-# envelope -- resultados possíveis para cada i
-
 def plotResults():
     #data = pd.read_csv('dtwResults1.csv');
     #data2 = pd.read_csv('pearsonResultsR1.csv');
-    data3 = pd.read_csv('miResults111.csv');
+    data3 = pd.read_csv('miHistResults.csv');
 
-    fig = px.line(data3, x = 'Intensity', y = ['Average Distance', 'Min Distance', 'Max Distance'], title = 'Mutual Information (Intensity x Correlation)');
+    fig = px.line(data3, x = 'Intensity', y = ['Average Correlation', 'Min Correlation', 'Max Correlation'], title = 'Mutual Information (Intensity x Correlation)');
 
     fig.show()
 
 
 def calcI(serie1, serie2, dtwResults, iValue):
     s1X, s1Y = serie1.addNoise(iValue)
-
     dtwAlignment = ClassicMethods.calcDTW(s1Y, serie2.serieY, False)
-
-    #ClassicMethods.plotSeries(s1X, s1Y);
-    #ClassicMethods.plotSeries(serie2.serieX, serie2.serieY);
-
     dtwResults.append(dtwAlignment)
 
 def calcPearson(serie1, serie2, pearsonResults, iValue):
@@ -53,14 +37,9 @@ def calcMi(serie1, serie2, miResults, iValue):
     s1X, s1Y = serie1.addNoise(iValue)
     
     #c_xy = np.histogram2d(s1Y, serie2.serieY, 8)[0]
-
     #mi = sm.mutual_info_score(None, None, contingency=c_xy)
 
     mi2 = mmmi._compute_mi_cc(s1Y, serie2.serieY, 4)   #k=4 || k=6
-
-    #print('Noise: ', iValue);
-    #print('Mutual info: ', mi)
-    #print('Histogram2d:\n', c_xy);
 
     #ClassicMethods.plotSeries(s1X, s1Y);
     #ClassicMethods.plotSeries(serie2.serieX, serie2.serieY);
@@ -101,6 +80,23 @@ def similaridadeXIntensidade(fileName, func): #pearson, mi, dtw
         data = [iValues[i], averageResults[i], minResults[i], maxResults[i]];
         writer.writerow(data);
 
+def main():
+    #similaridadeXIntensidade("dtwResults.csv", calcI)
+    #similaridadeXIntensidade("pearsonResults.csv", calcPearson)
+    #similaridadeXIntensidade("miResults.csv", calcMi);
+    plotResults();
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
 def testeSin():
     serie1 = TS.TemporalSerie()
     serie2 = TS.TemporalSerie()
@@ -140,21 +136,3 @@ def testeRand():
     #graphRand1 = DCTIF("timeSeriesRand.txt");
     #graphRand2 = DCSD("timeSeriesRand.txt");
     #graphRand3 = VG("timeSeriesRand.txt");
-
-
-def main():
-    #similaridadeXIntensidade("dtwResults.csv", calcI)
-    #similaridadeXIntensidade("pearsonResults.csv", calcPearson)
-    similaridadeXIntensidade("miResults111.csv", calcMi);
-    plotResults();
-    
-    
-    #dtwAlignment = ClassicMethods.calcDTW(x1, x2);
-    #print (dtwAlignment);
-    #ClassicMethods.statisticalSignificance(x1, x2);
-
-    pass
-
-
-if __name__ == "__main__":
-    main()
